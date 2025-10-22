12133fa1e86c6c006e54bac082fc162851015f97
Akshai-krishna-2003 <akshaykrishna1983@gmail.com>
2025-10-22 18:25:30 +0530

    fix: Infinite loading of app took place is fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practiceapp/src/app/app_router.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(ProviderScope(child: const MyApp()));
